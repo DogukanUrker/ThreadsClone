ff884f7a0b62510349751352234e4f01c5c07a1d
Doğukan Ürker <dogukanurker@icloud.com>
2023-08-13 00:27:14 +0300

    profile settings page added

--- clientDesktop/src/views/profile.tsx
+++ clientDesktop/src/views/profile.tsx
@@ -3,9 +3,14 @@ import axios from "axios";
 import { fetchUsername } from "../utils/auth";
 import { useNavigate } from "react-router";
 import { API } from "../dependencies";
-import { setPublic, setPrivate } from "../utils";
+import { setPublic, setPrivate, saveChanges } from "../utils";
 export default function Profile() {
   const [data, setData] = useState([]);
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [bio, setBio] = useState("");
+  const [link, setLink] = useState("");
+  const [mail, setMail] = useState("");
   const navigate = useNavigate();
   const Logout = () => {
     localStorage.removeItem("User");
@@ -35,27 +40,55 @@ export default function Profile() {
         value={data.id}
         className="block bg-transparent border-2 border-zinc-900 text-center p-2 my-4 mx-auto rounded-md focus:ring-2 focus:outline-none focus:ring-zinc-700"
       />
-      <input
-        type="text"
-        value={data.username}
-        className="block bg-transparent border-2 border-zinc-900 text-center p-2 my-4 mx-auto rounded-md focus:ring-2 focus:outline-none focus:ring-zinc-700"
-      />
-      <input
-        type="text"
-        value={data.name}
-        className="block bg-transparent border-2 border-zinc-900 text-center p-2 my-4 mx-auto rounded-md focus:ring-2 focus:outline-none focus:ring-zinc-700"
-      />
-      <input
-        type="text"
-        value={data.bio}
-        className="block bg-transparent border-2 border-zinc-900 text-center p-2 my-4 mx-auto rounded-md focus:ring-2 focus:outline-none focus:ring-zinc-700"
-      />
-      <input
-        type="text"
-        value={data.link}
-        className="block bg-transparent border-2 border-zinc-900 text-center p-2 my-4 mx-auto rounded-md focus:ring-2 focus:outline-none focus:ring-zinc-700"
-      />
-      <button className="block mx-auto py-3 px-6 mb-6 bg-gray-700 hover:bg-gray-800 text-sm text-white font-bold rounded-md transition duration-200">
+      <div className="w-fit mx-auto text-center my-4">
+        <p className="mb-1 font-medium text-slate-50/75">username</p>
+        <input
+          type="text"
+          value={data.username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="block bg-transparent border-2 border-zinc-900 text-center p-2 rounded-md focus:ring-2 focus:outline-none focus:ring-zinc-700"
+        />
+      </div>
+      <div className="w-fit mx-auto text-center my-4">
+        <p className="mb-1 font-medium text-slate-50/75">name</p>
+        <input
+          type="text"
+          value={data.name}
+          onChange={(e) => setName(e.target.value)}
+          className="block bg-transparent border-2 border-zinc-900 text-center p-2 rounded-md focus:ring-2 focus:outline-none focus:ring-zinc-700"
+        />
+      </div>
+      <div className="w-fit mx-auto text-center my-4">
+        <p className="mb-1 font-medium text-slate-50/75">mail</p>
+        <input
+          type="text"
+          value={data.email}
+          onChange={(e) => setMail(e.target.value)}
+          className="block bg-transparent border-2 border-zinc-900 text-center p-2 rounded-md focus:ring-2 focus:outline-none focus:ring-zinc-700"
+        />
+      </div>
+      <div className="w-fit mx-auto text-center my-4">
+        <p className="mb-1 font-medium text-slate-50/75">bio</p>
+        <input
+          type="text"
+          value={data.bio}
+          onChange={(e) => setBio(e.target.value)}
+          className="block bg-transparent border-2 border-zinc-900 text-center p-2 rounded-md focus:ring-2 focus:outline-none focus:ring-zinc-700"
+        />
+      </div>
+      <div className="w-fit mx-auto text-center my-4">
+        <p className="mb-1 font-medium text-slate-50/75">bio url</p>
+        <input
+          type="text"
+          value={data.link}
+          onChange={(e) => setLink(e.target.value)}
+          className="block bg-transparent border-2 border-zinc-900 text-center p-2 rounded-md focus:ring-2 focus:outline-none focus:ring-zinc-700"
+        />
+      </div>
+      <button
+        onClick={() => saveChanges(data.id, username, name, bio, link, mail)}
+        className="block mx-auto py-3 px-6 mb-6 bg-gray-700 hover:bg-gray-800 text-sm text-white font-bold rounded-md transition duration-200"
+      >
         Save Changes
       </button>
       {data.private ? (
@@ -64,7 +97,7 @@ export default function Profile() {
             onClick={() => setPublic(data.id)}
             className="block mx-auto py-3 px-6 bg-gray-700 hover:bg-gray-800 text-sm text-white font-bold rounded-md transition duration-200"
           >
-            set my account PUBLIC
+            set account visibility to PUBLIC
           </button>
         </div>
       ) : (
@@ -73,7 +106,7 @@ export default function Profile() {
             onClick={() => setPrivate(data.id)}
             className="block mx-auto py-3 px-6 bg-gray-700 hover:bg-gray-800 text-sm text-white font-bold rounded-md transition duration-200"
           >
-            set my account PRIVATE
+            set account visibility to PRIVATE
           </button>
         </div>
       )}
