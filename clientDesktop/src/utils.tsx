92fcfc720a2f425cc90167dd671609943c4b7561
Doğukan Ürker <dogukanurker@icloud.com>
2023-08-13 00:27:03 +0300

    saveChanges() added

--- clientDesktop/src/utils.tsx
+++ clientDesktop/src/utils.tsx
@@ -10,3 +10,22 @@ export function setPrivate(userID: string) {
   axios.put(`${API}setPrivate/${userID}`);
   window.location.reload();
 }
+
+export function saveChanges(
+  userID: string,
+  username: string,
+  name: string,
+  bio: string,
+  link: string,
+  mail: string
+) {
+  axios.put(`${API}updateUser/`, {
+    id: userID,
+    username: username,
+    name: name,
+    bio: bio,
+    link: link,
+    mail: mail,
+    profilePicture: "",
+  });
+}
