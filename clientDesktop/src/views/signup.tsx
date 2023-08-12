ea165be83b0b1298212cca5234e238d1c1539c66
Doğukan Ürker <dogukanurker@icloud.com>
2023-08-12 10:46:16 +0300

    theme changes in signup page

--- clientDesktop/src/views/signup.tsx
+++ clientDesktop/src/views/signup.tsx
@@ -47,33 +47,33 @@ export default function Signup() {
         {fetchToken() ? (
           <Navigate to="/profile" />
         ) : (
-          <div className="container mx-auto w-64 p-4 bg-zinc-800 rounded-2xl">
+          <div className="container mx-auto w-64 p-4  rounded-2xl">
             <form className="flex items-stretch flex-wrap">
               <input
                 type="text"
                 autoComplete="new-password"
-                className="block mx-auto text-center my-2 border-2 border-solid border-zinc-400 bg-zinc-800/50 text-white p-1 rounded-md transition outline-zinc-700 focus:outline-none focus:ring focus:ring-zinc-400"
+                className="block mx-auto text-center my-2 border-2 border-solid border-zinc-400 bg-transparent text-white p-1 rounded-md transition outline-zinc-700 focus:outline-none focus:ring focus:ring-zinc-400"
                 onChange={(event) => setUsername(event.target.value)}
                 placeholder="username"
               />
               <input
                 type="text"
                 autoComplete="new-password"
-                className="block mx-auto text-center my-2 border-2 border-solid border-zinc-400 bg-zinc-800/50 text-white p-1 rounded-md transition outline-zinc-700 focus:outline-none focus:ring focus:ring-zinc-400"
+                className="block mx-auto text-center my-2 border-2 border-solid border-zinc-400 bg-transparent text-white p-1 rounded-md transition outline-zinc-700 focus:outline-none focus:ring focus:ring-zinc-400"
                 onChange={(event) => setName(event.target.value)}
-                placeholder="real name"
+                placeholder="name"
               />
               <input
                 type="email"
                 autoComplete="new-password"
-                className="block mx-auto text-center my-2 border-2 border-solid border-zinc-400 bg-zinc-800/50 text-white p-1 rounded-md transition duration-200 outline-zinc-700 focus:outline-none focus:ring focus:ring-zinc-400"
+                className="block mx-auto text-center my-2 border-2 border-solid border-zinc-400 bg-transparent text-white p-1 rounded-md transition duration-200 outline-zinc-700 focus:outline-none focus:ring focus:ring-zinc-400"
                 onChange={(event) => setEmail(event.target.value)}
                 placeholder="email"
               />
               <input
                 type="password"
                 autoComplete="new-password"
-                className="block mx-auto text-center my-2 border-2 border-solid border-zinc-400 bg-zinc-800/50 text-white p-1 rounded-md transition duration-200 outline-zinc-700 focus:outline-none focus:ring focus:ring-zinc-400"
+                className="block mx-auto text-center my-2 border-2 border-solid border-zinc-400 bg-transparent text-white p-1 rounded-md transition duration-200 outline-zinc-700 focus:outline-none focus:ring focus:ring-zinc-400"
                 onChange={(event) => setPassword(event.target.value)}
                 placeholder="password"
               />
