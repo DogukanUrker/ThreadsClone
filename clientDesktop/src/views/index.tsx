0515d022461c4733279f89cf3cc8a0d08c4ce40f
Doğukan Ürker <dogukanurker@icloud.com>
2023-08-23 00:11:15 +0300

    added index page for testing

--- clientDesktop/src/views/index.tsx
+++ clientDesktop/src/views/index.tsx
@@ -1,7 +1,34 @@
-export default function Index() {
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { API } from "../dependencies";
+import { getNumberOfDays } from "../utils";
+
+export default function Links() {
+  let { userName } = useParams();
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get(`${API}fetchThreads/`).then((json) => setData(json.data));
+  }, []);
+  const renderThreads = () => {
+    return data.map((thread) => {
+      return (
+        <div className="m-8">
+          <div>{thread.author}</div>
+          <div>{thread.thread}</div>
+          <div>{getNumberOfDays(thread.date)}</div>
+        </div>
+      );
+    });
+  };
   return (
-    <div>
-      <p>Threads Clone App</p>
-    </div>
+    <>
+      <div>
+        <div className="w-fit">
+          <h1 className="font-semibold text-3xl mb-8 ">{userName}</h1>
+          {renderThreads()}
+        </div>
+      </div>
+    </>
   );
 }
